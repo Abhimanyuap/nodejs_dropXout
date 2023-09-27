cd2b9a05150f93b29ca20d46fb6266e83ab220b1
Abhimanyuap <97864658+Abhimanyuap@users.noreply.github.com>
2023-09-27 20:13:49 +0530

    first commit

---
+++ index.mjs
@@ -0,0 +1,17 @@
+// 1.module.js
+//  require("./module.js");
+
+//  2.module.js
+// const obj = require("./module");
+// console.log(obj);
+
+// const hi = require('./module');
+
+// hi.hello()
+
+// 3.example.mjs ==>
+
+import hello from "./example.mjs";
+
+hello()
+38min
---
+++ module.js
@@ -0,0 +1,18 @@
+// 1.
+// const sum = (a,b) => {
+//     return a+b;
+// } 
+// console.log(sum(2,3));
+
+// 2.
+// const a = 1;
+// const b = 2;
+// const c = 3;
+
+// module.exports = {a, b, c}; 
+
+// exports.hello = function(){
+//     console.log("hello");
+// }
+
+
